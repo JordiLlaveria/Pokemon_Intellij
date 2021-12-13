7fbb0502b655e72c955e1d98a7b64c87641c142b
Aidaapadilla <aida.padilla.gonzalez.aiko@gmail.com>
2021-12-13 19:34:43 +0100

    pagina web javascripts de shop.html, register.html i login.html

--- public/javascripts/shop.js
+++ public/javascripts/shop.js
@@ -1,4 +1,77 @@
 var BASE_URI = "http://localhost:8080/dsaApp"
 $(document).ready(function(){
     myStorage = window.localStorage;
-    $("#btnRegister").click(function(e){
+    var username = localStorage.getItem("username");
+    $("#buttonpokeball").click(function(){
+        $ajax({
+            headers: {'Accept': 'application/json',
+                'Content-Type': 'application/json'},
+                url: BASE_URI.concat("/auth/shop/buyObject"),
+                type: "POST",
+                data: JSON.stringify({"object": "pokeball", "username": username}),
+                dataType: 'json',
+                success: function(data) {
+                    alert("Superball correctly bought"); //necessitem dir-li les monedes que li queden
+                        window.location.href = "shop.html";
+                }
+                error: function(error){ //preguntar dema
+                    alert("You don't have enough money");
+                }
+        });
+
+    });
+    $("#buttonsuperball").click(function(){
+            $ajax({
+                headers: {'Accept': 'application/json',
+                    'Content-Type': 'application/json'},
+                    url: BASE_URI.concat("/auth/shop/buyObject"),
+                    type: "POST",
+                    data: JSON.stringify({"object": "superball", "username": username}),
+                    dataType: 'json',
+                    success: function(data) {
+                        alert("Superball correctly bought"); //necessitem dir-li les monedes que li queden
+                            window.location.href = "shop.html";
+                    }
+                    error: function(error){ //preguntar dema
+                        alert("You don't have enough money");
+                    }
+            });
+
+    });
+    $("#buttonpotion").click(function(){
+                $ajax({
+                    headers: {'Accept': 'application/json',
+                        'Content-Type': 'application/json'},
+                        url: BASE_URI.concat("/auth/shop/buyObject"),
+                        type: "POST",
+                        data: JSON.stringify({"object": "potion", "username": username}),
+                        dataType: 'json',
+                        success: function(data) {
+                            alert("Superball correctly bought"); //necessitem dir-li les monedes que li queden
+                                window.location.href = "shop.html";
+                        }
+                        error: function(error){ //preguntar dema
+                            alert("You don't have enough money");
+                        }
+                });
+
+    });
+    $("#buttonsuperpotion").click(function(){
+                $ajax({
+                    headers: {'Accept': 'application/json',
+                        'Content-Type': 'application/json'},
+                        url: BASE_URI.concat("/auth/shop/buyObject"),
+                        type: "POST",
+                        data: JSON.stringify({"object": "superpotion", "username": username}),
+                        dataType: 'json',
+                        success: function(data) {
+                            alert("Superball correctly bought"); //necessitem dir-li les monedes que li queden
+                                window.location.href = "shop.html";
+                        }
+                        error: function(error){ //preguntar dema
+                            alert("You don't have enough money");
+                        }
+                });
+
+    });
+});
