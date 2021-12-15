fbf4e3e8d328eeb95c51f6c41a22e8cfb9fc3166
Aidaapadilla <aida.padilla.gonzalez.aiko@gmail.com>
2021-12-15 16:30:19 +0100

    no detecta els radio buttons del registerCharacter.js

--- public/javascripts/registerCharacter.js
+++ public/javascripts/registerCharacter.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
     myStorage = window.localStorage;
     $("#btnRegister2").click(function(e){
         e.preventDefault();
-        var pokemon = $('#pokemons').val();
+        var pokemon = $('#pokemon').val();
         var character = $('#character').val();
-        var radioCharacter = document.getElementsByName("character"); //Check that the user choosed a character
-        var radioPokemon = document.getElementsByName("pokemons"); //Check that the user choosed a pokemon
+        var radioCharacter = document.getElementsByName("character").value; //Check that the user choosed a character
+        var radioPokemon = document.getElementsByName("pokemon").value; //Check that the user choosed a pokemon
 
         if(!(radioCharacter[0].checked || radioCharacter[1].checked || radioCharacter[2].checked)) {
                 alert("Please select your favorite character")
