12ed6727ce3135a09df6d9467047f6667bdcbc63
JordiLlaveria <jordi.llaveria@estudiantat.upc.edu>
2021-12-15 07:53:44 +0100

    Modificació queries

--- src/main/java/edu/upc/dsa/util/QueryHelper.java
+++ src/main/java/edu/upc/dsa/util/QueryHelper.java
@@ -8,8 +8,8 @@ public class QueryHelper {
 
         StringBuffer sb = new StringBuffer("INSERT INTO ");
         sb.append("pokemon.");
-        String nametable = entity.getClass().getSimpleName().substring(0,1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-        sb.append(nametable).append(" ");
+        //String nametable = entity.getClass().getSimpleName().substring(0,1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+        sb.append(entity.getClass().getSimpleName()).append(" ");
         sb.append("(");
 
         String [] fields = ObjectHelper.getFields(entity);
@@ -34,24 +34,24 @@ public class QueryHelper {
 
     public static String createQuerySELECT(Class theclass) {
         StringBuffer sb = new StringBuffer();
-        String nametable = theclass.getClass().getSimpleName().substring(0,1).toLowerCase() + theclass.getClass().getSimpleName().substring(1);
-        sb.append("SELECT * FROM ").append("pokemon.").append(nametable);
+        //String nametable = theclass.getClass().getSimpleName().substring(0,1).toLowerCase() + theclass.getClass().getSimpleName().substring(1);
+        sb.append("SELECT * FROM ").append("pokemon.").append(theclass.getSimpleName());
         sb.append(" WHERE name = ?");
 
         return sb.toString();
     }
     public static String createQuerySELECTALL(Class theclass) {
         StringBuffer sb = new StringBuffer();
-        String nametable = theclass.getSimpleName().substring(0,1).toLowerCase() + theclass.getClass().getSimpleName().substring(1);
-        sb.append("SELECT * FROM ").append("pokemon.").append(nametable);
+        //String nametable = theclass.getSimpleName().substring(0,1).toLowerCase() + theclass.getClass().getSimpleName().substring(1);
+        sb.append("SELECT * FROM ").append("pokemon.").append(theclass.getSimpleName());
 
         return sb.toString();
     }
 
     public static String createQueryDELETE(Object entity){
         StringBuffer sb = new StringBuffer();
-        String nametable = entity.getClass().getSimpleName().substring(0,1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-        sb.append("DELETE FROM ").append("pokemon.").append(nametable);
+        //String nametable = entity.getClass().getSimpleName().substring(0,1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+        sb.append("DELETE FROM ").append("pokemon.").append(entity.getClass().getSimpleName());
         sb.append(" WHERE name = ?");
 
         return sb.toString();
@@ -59,8 +59,8 @@ public class QueryHelper {
 
     public static String createQueryUPDATE(Object entity){
         StringBuffer sb = new StringBuffer("UPDATE ");
-        String nametable = entity.getClass().getSimpleName().substring(0,1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
-        sb.append(nametable).append(" ");
+        //String nametable = entity.getClass().getSimpleName().substring(0,1).toLowerCase() + entity.getClass().getSimpleName().substring(1);
+        sb.append(entity.getClass().getSimpleName()).append(" ");
         sb.append("SET ");
 
         String [] fields = ObjectHelper.getFields(entity);
