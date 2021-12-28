a361623665fc6734299b378685a0df50a2244763
RuthNB <73505235+RuthNB@users.noreply.github.com>
2021-12-29 00:47:30 +0100

    shop & web

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,8 +1,19 @@
 
 $(document).ready(function(){
+    $( ".input" ).focusin(function() {
+      $( this ).find( "span" ).animate({"opacity":"0"}, 200);
+    });
+
+    $( ".input" ).focusout(function() {
+      $( this ).find( "span" ).animate({"opacity":"1"}, 300);
+    });
     myStorage = window.localStorage;
-    $("#btnLogIn").click(function(e){
-    e.preventDefault();
+    $(".login").submit(function(){
+      $(this).find(".submit i").removeAttr('class').addClass("fa fa-check").css({"color":"#fff"});
+      $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
+      $(".feedback").show().animate({"opacity":"1", "bottom":"-80px"}, 400);
+      $("input").css({"border-color":"#2ecc71"});
+      e.preventDefault();
 
         var username = $('#username').val();
         var password = $('#password').val();
@@ -33,5 +44,9 @@ $(document).ready(function(){
 
     });
 });
+
+
+
+
 
 
